09f21a9ef5728d48b6c06569e0e25ecc615881b2
Chani Viner <czilbers@g.jct.ac.il>
2023-05-10 23:45:48 +0300

    5

--- src/main/java/com/CovidManagementSystem/CovidManagementSystem/CovidManagementSystemApplication.java
+++ src/main/java/com/CovidManagementSystem/CovidManagementSystem/CovidManagementSystemApplication.java
@@ -1,17 +1,18 @@
 package com.CovidManagementSystem.CovidManagementSystem;
 
+import com.CovidManagementSystem.CovidManagementSystem.model.Member;
+import com.CovidManagementSystem.CovidManagementSystem.service.MemberService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
+
 @SpringBootApplication
 @EnableMongoRepositories("com.CovidManagementSystem.CovidManagementSystem.repository")
 @ComponentScan("com.CovidManagementSystem.*")
 public class CovidManagementSystemApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(CovidManagementSystemApplication.class, args);
 	}
-
 }
