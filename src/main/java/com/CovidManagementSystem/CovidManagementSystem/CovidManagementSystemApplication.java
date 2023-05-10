9b16b5dc45029aa1a69709deb3bc18a53df85e76
Chani Viner <czilbers@g.jct.ac.il>
2023-05-10 14:52:10 +0300

    Initial commit

---
+++ src/main/java/com/CovidManagementSystem/CovidManagementSystem/CovidManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.CovidManagementSystem.CovidManagementSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CovidManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CovidManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/CovidManagementSystem/CovidManagementSystem/CovidManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CovidManagementSystem.CovidManagementSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CovidManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
